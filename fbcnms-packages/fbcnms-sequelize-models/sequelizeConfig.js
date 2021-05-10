79ec8e20e016d3312189bbcfc6d0f1c19553300c
mescorci <mescorci@everis.com>
2021-05-09 21:19:49 -0500

    Enable SSL connection
    Enable securely connection to mysql database (AZURE and GCP) by setting dialectOptions (ca, cert, key).
    For GCP You need to pass all 3 components, 2 certs and key file. Azure only require ca cert

--- fbcnms-packages/fbcnms-sequelize-models/sequelizeConfig.js
+++ fbcnms-packages/fbcnms-sequelize-models/sequelizeConfig.js
@@ -9,7 +9,7 @@
  */
 
 import type {Options} from 'sequelize';
-
+const fs=require('fs');
 // TODO: Pull from shared config
 const MYSQL_HOST = process.env.MYSQL_HOST || '127.0.0.1';
 const MYSQL_PORT = parseInt(process.env.MYSQL_PORT || '3306');
@@ -17,8 +17,54 @@ const MYSQL_USER = process.env.MYSQL_USER || 'root';
 const MYSQL_PASS = process.env.MYSQL_PASS || '';
 const MYSQL_DB = process.env.MYSQL_DB || 'cxl';
 const MYSQL_DIALECT = process.env.MYSQL_DIALECT || 'mysql';
+const CA_FILE = process.env.CA_FILE;
+const KEY_FILE = process.env.KEY_FILE;
+const CERT_FILE = process.env.CERT_FILE;
 
 const logger = require('@fbcnms/logging').getLogger(module);
+
+let ssl_required = false
+let CAcert = process.env.CA_FILE;
+let Ckey = process.env.KEY_FILE;
+let Ccert = process.env.CERT_FILE;
+let dialectOptions ={};
+
+if (process.env.CA_FILE) {
+  try {
+    CAcert = fs.readFileSync(CA_FILE, 'utf-8');
+    ssl_required = true;
+  } catch (e) {
+    console.warn('cannot read ca cert file', e);
+  }
+}
+
+if (process.env.KEY_FILE) {
+  try {
+    Ckey = fs.readFileSync(KEY_FILE, 'utf-8');
+    ssl_required = true;
+  } catch (e) {
+    console.warn('cannot read key file', e);
+  }
+}
+
+if (process.env.CERT_FILE) {
+  try {
+    Ccert = fs.readFileSync(CERT_FILE, 'utf-8');
+    ssl_required = true;
+  } catch (e) {
+    console.warn('cannot read cert file', e);
+  }
+}
+
+if (ssl_required) {
+  dialectOptions = {
+    "ssl": {
+      ca: CAcert,
+      key: Ckey,
+      cert: Ccert
+    }
+  };
+}
 
 const config: {[string]: Options} = {
   test: {
@@ -35,6 +81,8 @@ const config: {[string]: Options} = {
     host: MYSQL_HOST,
     port: MYSQL_PORT,
     dialect: MYSQL_DIALECT,
+    ssl: ssl_required,
+    dialectOptions,
     logging: (msg: string) => logger.debug(msg),
   },
   production: {
@@ -44,6 +92,8 @@ const config: {[string]: Options} = {
     host: MYSQL_HOST,
     port: MYSQL_PORT,
     dialect: MYSQL_DIALECT,
+    ssl: ssl_required,
+    dialectOptions,
     logging: (msg: string) => logger.debug(msg),
   },
 };
